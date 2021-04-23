7c06d02d7f8fd5756660e95b4ac991629b8d3ce1
s3545999 <s3545999@student.rmit.edu.au>
2021-04-23 14:27:28 +1000

    Adding getters and setters for player fields

--- Player.h
+++ Player.h
@@ -4,10 +4,14 @@
 class Player{
 public:
    Player(std::string name);
+   void setName(std::string newName);
+   std::string getName();
+   void addScore(int scoreToAdd);
+   int getScore();
 private:
-   
    std::string name;
    int score;
+   // Bag pointer for hand(when bag implemented)
 };
 
 # endif //PLAYER_H
