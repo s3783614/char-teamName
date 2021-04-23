60ca202883afd2563fa20893731fa1ed59b8f608
s3545999 <s3545999@student.rmit.edu.au>
2021-04-23 14:28:13 +1000

    Player.h update

--- Player.cpp
+++ Player.cpp
@@ -1,5 +1,5 @@
-#include <string>
 #include "player.h"
+#include <iostream>
 
 Player::Player(std::string name)
 {
@@ -9,5 +9,31 @@ Player::Player(std::string name)
 
 Player::~Player()
 {
-   
+
 }
+
+// Update the name of a player with a string input
+void Player::setName(std::string newName)
+{
+   this->name = newName;
+}
+
+// Return a string of the players name
+std::string Player::getName()
+{
+   return this->name;
+}
+
+// Increase the players score by an int input
+void Player::addScore(int scoreToAdd)
+{
+   this->score += scoreToAdd;
+}
+
+// Return the score of the player as an int
+int Player::getScore()
+{
+   return this->score;
+}
+
+// Needs to have play tile etc after bag implemented
